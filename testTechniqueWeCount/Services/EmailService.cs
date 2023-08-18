fcc7698d7818ee04730c74e41a8d8efc50add40f
amine lgdi <amine.laaguidi.9@gmail.com>
2023-08-18 15:03:24 +0100

    email send service

---
+++ testTechniqueWeCount/Services/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Mail;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace testTechniqueWeCount.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            var smtpServer = emailSettings["SmtpServer"];
+            var port = Convert.ToInt32(emailSettings["Port"]);
+            var username = emailSettings["Username"];
+            var password = emailSettings["Password"];
+            var fromEmail = emailSettings["FromEmail"];
+
+            using (var client = new SmtpClient(smtpServer, port))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(username, password);
+                client.EnableSsl = true;
+                var message = new MailMessage(fromEmail, toEmail, subject, body);
+                await client.SendMailAsync(message);
+            }
+        }
+    }
+}
