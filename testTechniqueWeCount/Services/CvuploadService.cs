42c9e8e744b1703f98d7e75d98cafb420bc5eb13
amine lgdi <amine.laaguidi.9@gmail.com>
2023-08-18 15:02:25 +0100

    Cv upload service

---
+++ testTechniqueWeCount/Services/CvuploadService.cs
@@ -0,0 +1,30 @@
+﻿namespace testTechniqueWeCount.Services
+{
+    public class CvuploadService : ICvuploadService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CvuploadService(IWebHostEnvironment webHostEnvironment) {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadCvAsync(string fullName, IFormFile cv)
+        {
+            if (cv == null || cv.Length == 0)
+            {
+                return null;
+            }
+
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(cv.FileName);
+            string userFolder = Path.Combine(_webHostEnvironment.ContentRootPath, "Uploads", fullName);
+            Directory.CreateDirectory(userFolder);
+
+            string filePath = Path.Combine(userFolder, fileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await cv.CopyToAsync(stream);
+            }
+
+            return Path.Combine(fullName, fileName);
+        }
+    }
+ }
---
+++ testTechniqueWeCount/Services/ICvuploadService.cs
@@ -0,0 +1,7 @@
+﻿namespace testTechniqueWeCount.Services
+{
+    public interface ICvuploadService
+    {
+        public Task<string> UploadCvAsync(string fullName, IFormFile cv);
+    }
+}
